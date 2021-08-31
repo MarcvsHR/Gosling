1d9ef028c2b0996339f352d491ae853395e381f3
prodotes <nguscic@gmail.com>
2021-08-31 16:19:20 +0200

    Added new window

--- src/main/java/prodo/marc/gosling/controllers/HelloController.java
+++ src/main/java/prodo/marc/gosling/controllers/HelloController.java
@@ -1,14 +1,33 @@
 package prodo.marc.gosling.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import prodo.marc.gosling.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    Button songViewButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to Gosling");
     }
+
+    @FXML
+    protected void showSongWindow() throws IOException  {
+        Stage stage = (Stage) songViewButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/song-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Songs");
+        stage.setScene(scene);
+    }
+
 }
